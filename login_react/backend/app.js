a28630a3fd8b6c3cb4ba11e82340e433265afb68
MatiasOrrego <orregomatias50@gmail.com>
2024-11-01 12:44:57 -0300

    funciona todo correctamente

--- login_react/backend/app.js
+++ login_react/backend/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import userRoutes from './src/routes/userRoutes.js';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+}));
 app.use('/api', userRoutes);
 
 app.listen(port, () => {
